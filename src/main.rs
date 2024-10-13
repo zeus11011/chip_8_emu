e4aee38ac7eb5d4faf1983f82ba39d0df27782f7
Zeus11011 <naikanish13@gmail.com>
2024-10-13 23:03:05 +0530

    added argument parser

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,41 @@ mod display;
 // use crate::display;
 // mod keyboard;
 
-use std::{char, fs, time::Instant};
+use std::{
+    fs,
+    path::{PathBuf},
+    time::Instant,
+};
 mod cpu;
-use macroquad::prelude::*;
+use macroquad::{prelude::*};
 mod keyboard;
+
+use argh::FromArgs;
+
+#[derive(FromArgs)]
+/// Chip-8 Emulator implementation in rust
+struct Argumentparser {
+    ///  file Path of the rom
+    #[argh(option, short = 'f')]
+    file_path: String,
+}
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
+    let arg: Argumentparser = argh::from_env();
+    let file_path = PathBuf::from(arg.file_path);
+    if !file_path.exists() {
+        panic!("File could not be found")
+    }
+
+    let rom = fs::read(file_path).unwrap();
     let mut current_time = Instant::now();
     let mut chip_8 = cpu::Cpu::default();
-    let rom = fs::read("/home/zeus/Documents/roms/chip8-roms/games/Tank.ch8").unwrap();
-    chip_8.read_rom(rom);
-    // loop {
 
-    // }
+    chip_8.read_rom(rom);
     request_new_screen_size(64.0 * 20.0, 32.0 * 20.0);
     clear_background(BLACK);
+
     loop {
         if current_time.elapsed().as_millis() > 17 {
             chip_8.cycle().await;
